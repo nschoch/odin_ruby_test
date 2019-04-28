093a9c65629fcc8431cbfb1aea675af28b8a2aed
Nick Schoch <nschoch@gmail.com>
2019-04-28 20:39:44 +0000

    stock picker complete

---
+++ stock_picker.rb
@@ -0,0 +1,18 @@
+def stock_picker(prices)
+  best_combination = []
+  max_proceeds = 0
+  place_count = 0
+  prices[0..-2].each do |x|
+    prices[place_count+1...].each do |z|
+      # puts "X: #{x} Z: #{z} X-Z $#{z-x}"
+      if ( (z-x) > max_proceeds )
+        max_proceeds = z - x
+        best_combination = [place_count, prices[place_count...].find_index(z)]
+      end
+    end
+    place_count += 1
+  end
+  best_combination
+end
+
+puts stock_picker([17,3,6,9,15,8,6,1,10]).inspect
