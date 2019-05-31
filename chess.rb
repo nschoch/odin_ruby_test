c3c4db58ed0e7ea8420c781a7b05c14ef07d2435
Nick Schoch <nschoch@gmail.com>
2019-05-31 10:48:02 -0700

    create chess file with class outlinel

---
+++ chess.rb
@@ -0,0 +1,29 @@
+class Game
+end
+
+class Board
+end
+
+class Piece
+  
+end
+
+class Pawn < Piece
+end
+
+class Rook < Piece
+end
+
+class Knight < Piece
+end
+
+class Bishop < Piece
+end
+
+class Queen < Piece
+end
+
+class King < Piece
+end
+
+
